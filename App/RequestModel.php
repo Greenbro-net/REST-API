583d939e8cc98d378c4a518675f88bf2cc145d9a
Surf-house <reeeee@ukr.net>
2021-06-26 12:58:08 +0300

    public_html

--- App/RequestModel.php
+++ App/RequestModel.php
@@ -4,11 +4,7 @@ namespace App;
 
 use PDO;
 
-define("DATABASE_HOST1", "hosting26.ukrnames.com");
-define("DATABASE_HOST", "greenbro.net");
-define("DATABASE_USERNAME", "green64_one");
-define("DATABASE_PASSWORD", "1q2w3e4r5t6y7u8i123");
-define("DATABASE_NAME", "green64_work_tasks");
+require_once 'config.php';
 
 
 
